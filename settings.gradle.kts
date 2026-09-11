0f3a3875c8fd5c6c34e2f185b8dffd45ded1d960
natdanainet8911-png <natdanai.net8911@ndlp.go.th>
2026-09-11 15:59:15 +0700

    Initialize settings.gradle.kts for AuroraPlayer

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+rootProject.name = "AuroraPlayer"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google(); mavenCentral(); gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google(); mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+include(":composeApp")
