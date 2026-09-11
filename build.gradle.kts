7b74b9707681b1dc0220076b75ab1d90efc4e477
natdanainet8911-png <natdanai.net8911@ndlp.go.th>
2026-09-11 16:12:08 +0700

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)  apply false
+    alias(libs.plugins.androidApplication)   apply false
+    alias(libs.plugins.composeMultiplatform) apply false
+    alias(libs.plugins.composeCompiler)      apply false
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
